a64863ad41bd3f59e73b144d2e05c93424391fbf
zivkaziv <zivkalderon95@gmail.com>
2020-08-28 21:55:58 +0300

    adding logs

--- scraping/scrape.js
+++ scraping/scrape.js
@@ -16,16 +16,22 @@ const chromeOptions = {
 };
 
 const scrape = async (id) => {
+	console.log(`${id} - Start sraping`);
 	puppeteer.use(StealthPlugin());
 	const browser = await puppeteer.launch(chromeOptions);
+	console.log(`${id} - puppeteer.launch`);
 	const page = await browser.newPage();
+	console.log(`${id} - browser.newPage`);
 	await page.setDefaultNavigationTimeout(0);
 	await page.goto("https://nacionalidade.justica.gov.pt/");
+	console.log(`${id} - open website`);
 	await page.waitFor(1500);
 	//var cookiesToDelete = await page._client.send('Network.getAllCookies');
 	await page.type("#SenhaAcesso", id);
+	console.log(`${id} - type id`);
 	await page.waitFor(1000);
 	const results = await solve(page);
+	console.log(`${id} - solve captcha`);
 	if (results.error) {
 		await browser.close();
 		return results.errorMessage || "Failed to scrape";
@@ -34,6 +40,7 @@ const scrape = async (id) => {
 	await page.click("#btnPesquisa");
 	await page.waitFor(1000);
 	await page.screenshot({ path: `${id}.png`, fullPage: true });
+	console.log(`${id} - taking screenshot`);
 	await browser.close();
 };
 
