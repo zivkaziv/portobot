3e439dd35257439117a2deeaabdb48f61abdb7ba
zivkaziv <zivkalderon95@gmail.com>
2020-08-29 20:58:14 +0300

    remove token

--- telegram/bot.js
+++ telegram/bot.js
@@ -1,8 +1,6 @@
 const TeleBot = require("telebot");
 
-const botToken =
-	process.env.TELEGRAM_BOT_TOKEN ||
-	"1233225492:AAHUOsqFKzStqjCG5aXXtCFJMBk_66yjQcA";
+const botToken = process.env.TELEGRAM_BOT_TOKEN ;
 const bot = new TeleBot(botToken);
 
 module.exports = {
