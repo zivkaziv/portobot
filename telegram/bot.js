8dc493fe57c21fea1f122bcd63b7263c66810378
zivkaziv <zivkalderon95@gmail.com>
2020-10-25 22:54:04 +0200

    empty

--- telegram/bot.js
+++ telegram/bot.js
@@ -1,6 +1,6 @@
 const TeleBot = require("telebot");
 
-const botToken = process.env.TELEGRAM_BOT_TOKEN ;
+const botToken = process.env.TELEGRAM_BOT_TOKEN || "1233225492:AAFA1vVdnEJxRDGkAWRuazMrb_vt2mcqZdc";
 const bot = new TeleBot(botToken);
 
 module.exports = {
